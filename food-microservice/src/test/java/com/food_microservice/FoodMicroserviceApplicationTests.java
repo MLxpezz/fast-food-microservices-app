2b0ad9963f8f145b711b8bcb29ecb87d972d68ed
MLxpezz <mlopez_098@hotmail.com>
2024-11-27 08:49:57 -0600

    first structure

---
+++ food-microservice/src/test/java/com/food_microservice/FoodMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.food_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
