264dc79019688d78a88357b4c1e833e2617d5697
MLxpezz <mauricio_lopez_098@hotmail.com>
2024-12-19 21:20:29 -0600

    add address & phone service

--- users-microservice/src/main/java/com/users_microservice/service/interfaces/IAddressService.java
+++ users-microservice/src/main/java/com/users_microservice/service/interfaces/IAddressService.java
@@ -1,4 +1,16 @@
 package com.users_microservice.service.interfaces;
 
+import com.users_microservice.dto.AddressDTO;
+
+import java.util.List;
+
 public interface IAddressService {
+
+    void saveAddress(String address);
+
+    List<AddressDTO> getAddresses();
+
+    void updateAddress(Long id, AddressDTO address);
+
+    void deleteAddress(Long id);
 }
--- users-microservice/src/main/java/com/users_microservice/service/interfaces/IPhoneService.java
+++ users-microservice/src/main/java/com/users_microservice/service/interfaces/IPhoneService.java
@@ -1,4 +1,16 @@
 package com.users_microservice.service.interfaces;
 
+import com.users_microservice.dto.PhoneDTO;
+
+import java.util.List;
+
 public interface IPhoneService {
+
+    void savePhone(String phone);
+
+    List<PhoneDTO> getPhones();
+
+    void deletePhone(Long id);
+
+    void updatePhone(Long id, PhoneDTO phoneDTO);
 }
