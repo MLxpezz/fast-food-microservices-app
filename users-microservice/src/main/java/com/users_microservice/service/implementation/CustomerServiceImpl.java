46d2bd7e2b0aad8475c4aa18b70e3cfeeaad6bd5
MLxpezz <mauricio_lopez_098@hotmail.com>
2024-12-29 10:46:47 -0600

    add route for users-msvc in gateway api

--- users-microservice/src/main/java/com/users_microservice/service/implementation/CustomerServiceImpl.java
+++ users-microservice/src/main/java/com/users_microservice/service/implementation/CustomerServiceImpl.java
@@ -19,7 +19,6 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 @Service
 public class CustomerServiceImpl implements ICustomerService {
